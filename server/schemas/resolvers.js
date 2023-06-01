e73cea6a8c74f0cb218ecee791d7bb7b68d7d554
Hunter Wilson <huntertheguy123@gmail.com>
2023-06-01 16:32:18 -0500

    added comments

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,14 +4,17 @@ const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
+    //retrieves all users
     users: async () => {
       return User.find();
     },
 
+    //retrieves a specific user
     user: async (parent, { userId }) => {
       return User.findOne({ _id: userId });
     },
 
+    //retrieves user info by id from context
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id });
@@ -21,12 +24,16 @@ const resolvers = {
   },
 
   Mutation: {
+    
+    //create new user with name,email,password and generates token
     addUser: async (parent, { name, email, password }) => {
       const user = await User.create({ name, email, password });
       const token = signToken(user);
 
       return { token, user };
     },
+
+    //find user by email
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
@@ -34,12 +41,14 @@ const resolvers = {
         throw new AuthenticationError("No user with this email");
       }
 
+      //checks is password is correct
       const correctPassword = await user.isCorrectPassword(password);
 
       if (!correctPassword) {
         throw new AuthenticationError("Incorrect password");
       }
 
+      //generate toke for the user and returns user and token
       const token = signToken(user);
       return { token, user };
     },
--- server/utils/auth.js
+++ server/utils/auth.js
@@ -5,16 +5,18 @@ const expiration = "1h";
 
 module.exports = {
   authMiddleware: function ({ req }) {
-    let token = req.body.token || req.query.toke || req.headers.authorization;
+    let token = req.body.token || req.query.token || req.headers.authorization;
 
     if (req.headers.authorization) {
       token = token.split(" ").pop.trim();
     }
 
+    //if no token found, request object as is
     if (!token) {
       return req;
     }
 
+    //set user on request object with data taken from token
     try {
       const { data } = jwt.verify(token, secret, { maxAge: expiration });
       req.user = data;
@@ -24,6 +26,8 @@ module.exports = {
 
     return req;
   },
+
+  //create payload containing user data and signs payload with a secret key and expiration
   signToken: function ({ email, name, _id }) {
     const payload = { email, name, _id };
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
