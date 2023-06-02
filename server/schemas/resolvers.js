c31824fe9995513a526c9d76bd0d28aa09172663
Hunter Wilson <huntertheguy123@gmail.com>
2023-06-02 14:56:42 -0500

    add to resolvers and typdefs, got graphql server running

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,7 @@
 const { AuthenticationError } = require("apollo-server-express");
-const { User } = require("../models");
+const User = require("../models/user");
 const { signToken } = require("../utils/auth");
+const Task = require("../models/task");
 
 const resolvers = {
   Query: {
@@ -9,10 +10,10 @@ const resolvers = {
       return User.find();
     },
 
-    //retrieves a specific user
+/*     //retrieves a specific user
     user: async (parent, { userId }) => {
       return User.findOne({ _id: userId });
-    },
+    }, */
 
     //retrieves user info by id from context
     me: async (parent, args, context) => {
@@ -21,10 +22,31 @@ const resolvers = {
       }
       throw new AuthenticationError("You need to log in");
     },
+
+    //easier on frontend to filter out tasks
+    /* randomTask: async (parent, args, context) => {
+      if (context.user) {
+        //first checks if an authenticated user and finds user based off of context
+        const user = await User.findOne({ _id: context.user._id });
+        //finds all tasks that have not been completed
+        const tasks = await Task.find({ completed: false });
+        //randomizes tasks
+        const random = Math.floor(Math.random() * tasks.length);
+        const ranTask = tasks[random];
+        
+        //push random task into array
+        //create an empty array
+        //add for loop to return
+
+        await user.save();
+        return ranTask;
+      }
+      throw new AuthenticationError("You need to log in");
+    },
+     */
   },
 
   Mutation: {
-    
     //create new user with name,email,password and generates token
     addUser: async (parent, { name, email, password }) => {
       const user = await User.create({ name, email, password });
@@ -42,7 +64,7 @@ const resolvers = {
       }
 
       //checks is password is correct
-      const correctPassword = await user.isCorrectPassword(password);
+      const correctPassword = await user.comparePasswords(password);
 
       if (!correctPassword) {
         throw new AuthenticationError("Incorrect password");
@@ -52,6 +74,43 @@ const resolvers = {
       const token = signToken(user);
       return { token, user };
     },
+
+/*     completeTask: async (parent, { taskId }, context) => {
+        if (context.user) {
+            
+            //find user based off of authenticated id
+            const user = await User.findOneAndUpdate({ _id: context.user.id })
+            
+            //checks to make sure task is in user's array
+            const task = user.tasks.indexOf(taskId);
+
+            //if task is not found then throw error
+            if (task === -1 ) {
+                throw new Error("No tasks found")
+            }
+
+            //removes from user array
+            user.tasks.splice(task, 1);
+
+            //adds count to user completed tasks
+            user.findByIdAndUpdate += 1;
+
+            //if user has completed all the tasks then sets them all back to false
+            if (user.tasks.length === 0 ) {
+                await Task.updateMany({}, { completed: false });
+            }
+
+            //saves updated user
+            await user.save();
+
+            //find task by id and mark comleted
+            const updatedTask = await Task.findOneAndUpdate(
+                { _id: taskId },
+                { completed: true }
+            );
+            return updatedTask;
+        };
+    } */
   },
 };
 
---
+++ server/schemas/typedefs.js
@@ -0,0 +1,34 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+ type User {
+    id: ID!
+    name: String!
+    email: String!
+    tasks: [ID]!
+    completedTasks: Int!
+ }
+
+ type Task {
+    id: ID!
+    statement: String!
+    completed: Boolean!
+ }
+ 
+ type Auth{
+    token: ID!
+    user: User
+ }
+
+type Query{
+    users: [User]
+    me: User
+}
+
+type Mutation{
+    addUser (name: String!, email: String!, password: String!): Auth
+    login (email: String!, password: String!): Auth
+}
+`;
+
+module.exports = typeDefs;
