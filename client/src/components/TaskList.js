b30f62573ea2633481b3f6127a29e8b4783f5432
Michael <michaelmees99@gmail.com>
2023-06-06 08:47:07 -0400

    EXPIREMENTAL BUILD
    using on mouse down and mouse up effects along with custom css properties, the user now has to click and hold on a task for 2 seconds for it to be considered complete. holding the button creates a full up bar that the user can visibly see until its complete

--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,13 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
+import './TaskList.css';
 
 const TaskList = () => {
   const tasks = ['Brush Your teeth', 'Make Your Bed', 'Take a Walk', 'Breathing Exercise'];
   const [checkedTasks, setCheckedTasks] = useState(Array(tasks.length).fill(false));
+  const [pressedTasks, setPressedTasks] = useState(Array(tasks.length).fill(false));
+  const timers = useRef([]);
 
-  const toggleTask = index => {
-    const newCheckedTasks = [...checkedTasks];
-    newCheckedTasks[index] = !newCheckedTasks[index];
-    setCheckedTasks(newCheckedTasks);
+  const startTimer = index => {
+    const newPressedTasks = [...pressedTasks];
+    newPressedTasks[index] = true;
+    setPressedTasks(newPressedTasks);
+
+    timers.current[index] = setTimeout(() => {
+      const newCheckedTasks = [...checkedTasks];
+      newCheckedTasks[index] = true;
+      setCheckedTasks(newCheckedTasks);
+    }, 2000);
+  };
+
+  const clearTimer = index => {
+    clearTimeout(timers.current[index]);
+
+    const newPressedTasks = [...pressedTasks];
+    newPressedTasks[index] = false;
+    setPressedTasks(newPressedTasks);
   };
 
   return (
@@ -15,8 +32,10 @@ const TaskList = () => {
       {tasks.map((task, index) => (
         <li key={index} className="mb-2">
           <button 
-            onClick={() => toggleTask(index)}
-            className={`font-mono text-2xl text-left w-full py-2 rounded shadow transition-colors duration-300 ${checkedTasks[index] ? 'bg-green-300' : 'hover:bg-blue-300'}`}
+            onMouseDown={() => startTimer(index)}
+            onMouseUp={() => clearTimer(index)}
+            onMouseLeave={() => clearTimer(index)}
+            className={`task-button font-mono text-2xl text-left w-full py-2 rounded shadow transition-colors duration-300 ${checkedTasks[index] ? 'bg-green-300' : 'hover:bg-blue-300'} ${pressedTasks[index] ? 'pressed' : ''}`}
           >
             {checkedTasks[index] && <span className="mr-2 text-white text-2xl">✓</span>}
             <span className={checkedTasks[index] ? 'line-through text-white' : ''}>{task}</span>
