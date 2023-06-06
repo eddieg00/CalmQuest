f010abdfe38a2e8f6498168036e3353bc31a66a6
Michael <michaelmees99@gmail.com>
2023-06-05 20:41:23 -0400

    Revert "Revert "dynamic tasklist prototype""
    This reverts commit 49e7bdf45c764ae5397e80f04e8662d209714944.

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Login from './components/Login';
 import { Home } from './components/Home';
+import TaskList from './components/TaskList';
 
 const App = () => {
     return (
