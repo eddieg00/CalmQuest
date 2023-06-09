f89d4e3aa444a8706bc5d0c1313de0a3659a94fc
DylanCaff <dylancaff0902@gmail.com>
2023-06-09 16:27:57 -0400

    Update Home.js

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -62,63 +62,64 @@ export const Home = () => {
   };
 
   return (
-    <div className="flex justify-center items-center h-screen bg-gradient-to-t from-green-300 via-blue-500 to-purple-600">
-      <div className="flex flex-col justify-center items-center">
-      <header className="py-4 md:py-8 px-4 md:px-8 flex justify-between items-center">
-    <h2 className="text-3xl text-emerald-400 font-nexa text-center font-bold">CalmQuest</h2>
-    <div className="flex items-center ml-auto">
-      <button
-        className="rounded-2xl text-white bg-blue-400 px-2 py-2 shadow-md hover:text-blue-400 hover:bg-white transition duration-200 ease-in"
-        onClick={openModal}
-      >
-        Profile
-      </button>
-    </div>
-  </header>
-
-        <div className="flex flex-col md:flex-row">
-          <div className="bg-white md:w-3/10 p-4 md:p-8 flex flex-col justify-between">
-            <div>
-              <HealthResourceList
-                healthResources={healthResources}
+    <body className="flex flex-col w-screen min-h-screen bg-gradient-to-t from-green-300 via-blue-500 to-purple-600">
+      <div className="flex flex-grow">
+        <div className="bg-white w-3/10 p-8 flex flex-col justify-between h-screen">
+          <div>
+            <h2 className="text-3xl mb-4 text-emerald-400 font-nexa font-bold">CalmQuest</h2>
+            <button className="rounded-2xl m-2 text-white bg-blue-400 w-2/5 px-2 py-2 shadow-md hover:text-blue-400 hover:bg-white transition duration-200 ease-in" 
+                onClick={openModal}>Profile
+            </button>
+              <div className='flex'>
+              <HealthResourceList 
+                healthResources={healthResources} 
                 handleResourceClick={handleResourceClick}
                 handleManageAnxiety={handleManageAnxiety}
                 handleKeepHeartHealthy={handleKeepHeartHealthy}
                 handleGetEnoughSleep={handleGetEnoughSleep}
                 handleGetActive={handleGetActive}
                 handleChoosingDoctor={handleChoosingDoctor}
-                search={search}
+                search={search} 
                 handleSearch={handleSearch}
               />
-            </div>
-            <p className="text-center md:text-left">Logout</p>
+              </div>
           </div>
+          <p>Logout</p>
+        </div>
 
-          <div className="w-full md:w-7/10 flex flex-col items-center text-center md:items-start justify-start p-4 md:p-8">
-            <div className="bg-white rounded-lg shadow p-4 md:p-8 w-full overflow-auto mb-6">
-              <h1 className="text-4xl mb-4 text-center justify-center md:text-left">
-                Welcome to <span className="justify-center text-center font-nexa font-bold text-emerald-400">CalmQuest</span>
-              </h1>
-              <p className="text-2xl mb-4 text-center md:text-left bg-gradient-to-l from-emerald-600 via-emerald-500 to-emerald-600 bg-clip-text text-transparent font-nexa font-bold">
-                Daily Quests:
-              </p>
-              <TaskList />
-            </div>
-            <div className="bg-white rounded-lg shadow p-4 md:p-8 w-full mb-4">
-              <p className="text-2xl mb-4 text-center md:text-left bg-gradient-to-l from-emerald-600 via-emerald-500 to-emerald-600 bg-clip-text text-transparent font-nexa font-bold">
-                Today's Affirming Quote:
-              </p>
-              <p className="font-nexa font-ultralight text-3xl italic text-center">
+        <div className="w-full w-7/10 flex flex-col items-start justify-start p-8">
+          <div className="bg-white rounded-lg shadow p-8 w-full overflow-auto mb-6">
+            <h1 className="text-4xl mb-4 text-center">Welcome to <span className="font-nexa font-bold text-emerald-400">CalmQuest</span></h1>
+            <p className="text-2xl text-center justify-center bg-gradient-to-l from-emerald-600 via-emerald-500 to-emerald-600 bg-clip-text text-transparent font-nexa font-bold">Daily Quests:</p>
+            <TaskList />
+          </div>
+          <div className=" w-full p-8 bg-white rounded-lg shadow">
+            <p className="text-2xl text-center bg-gradient-to-l from-emerald-600 via-emerald-500 to-emerald-600 bg-clip-text text-transparent font-nexa font-bold">Today's Affirming Quote:</p>
+            <p className="font-nexa font-ultralight text-3xl italic text-center ">
                 {quoteData.quote ? `“${quoteData.quote}” - ${quoteData.author}` : 'Loading...'}
-              </p>
-            </div>
+            </p>
           </div>
         </div>
       </div>
 
       {modalOpen && (
-        <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-black bg-opacity-50">
-          <section className="bg-grey-400 flex font-medium items-center justify-center h-screen">
+        <div
+          style={{
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'center',
+            background: 'rgba(0, 0, 0, 0.5)',
+          }}
+        >
+          <section
+            style={{ fontFamily: 'Montserrat' }}
+            className="bg-grey-400 flex font-medium items-center justify-center h-screen"
+          >
             <section className="w-64 mx-auto bg-teal-700 rounded-2xl px-8 py-6 shadow-lg">
               <div className="flex items-center justify-between">
                 <span className="text-white">
@@ -165,7 +166,7 @@ export const Home = () => {
           </section>
         </div>
       )}
-    </div>
+    </body>
   );
 };
 
