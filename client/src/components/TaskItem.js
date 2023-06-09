9d4447ee7e701c2ca5ea7d1b9a7e65cacc1a4c75
DylanCaff <dylancaff0902@gmail.com>
2023-06-09 17:39:43 -0400

    made buttons work

--- client/src/components/TaskItem.js
+++ client/src/components/TaskItem.js
@@ -2,18 +2,18 @@ import React, { useState, useEffect } from 'react';
 import { useSpring, animated } from 'react-spring';
 
 const TaskItem = ({ task, index, checkedTasks, setCheckedTasks }) => {
-  const [isMouseDown, setIsMouseDown] = useState(false);
+  const [isTouchDown, setIsTouchDown] = useState(false);
   const [timerId, setTimerId] = useState(null);
   const [isChecked, setIsChecked] = useState(false);
   const fillAnimation = useSpring({
-    width: isMouseDown ? '100%' : isChecked ? '100%' : '0%',
-    backgroundColor: isMouseDown ? 'rgba(0, 255, 0, 0.2)' : isChecked ? 'rgba(0, 255, 0, 0.2)' : 'transparent',
+    width: isTouchDown ? '100%' : isChecked ? '100%' : '0%',
+    backgroundColor: isTouchDown ? 'rgba(0, 255, 0, 0.2)' : isChecked ? 'rgba(0, 255, 0, 0.2)' : 'transparent',
     config: { duration: 1500 },
-    immediate: !isMouseDown,
+    immediate: !isTouchDown,
   });
 
   useEffect(() => {
-    if (isMouseDown) {
+    if (isTouchDown) {
       setTimerId(setTimeout(() => {
         setIsChecked(true);
         const newCheckedTasks = [...checkedTasks];
@@ -25,29 +25,35 @@ const TaskItem = ({ task, index, checkedTasks, setCheckedTasks }) => {
       clearTimeout(timerId);
     };
   // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [isMouseDown, index, checkedTasks]);
+  }, [isTouchDown, index, checkedTasks]);
 
-  const handleMouseDown = () => {
-    setIsMouseDown(true);
+  const handleTouchStart = () => {
+    setIsTouchDown(true);
   };
 
-  const handleMouseUp = () => {
-    setIsMouseDown(false);
+  const handleTouchEnd = () => {
+    setIsTouchDown(false);
     clearTimeout(timerId);
   };
 
+  const buttonClassName = `relative overflow-hidden font-mono text-2xl text-left w-full py-2 rounded shadow transition-colors duration-300 border border-black ${
+    checkedTasks[index] ? 'bg-green-300' : 'hover:bg-stone-200'
+  } transform transition-transform duration-200 ease-in-out hover:scale-105`;
+
   return (
     <li className="mb-2">
-      <button 
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        onMouseLeave={handleMouseUp}
-        className={`relative overflow-hidden font-mono text-2xl text-left w-full py-2 rounded shadow transition-colors duration-300 border border-black ${checkedTasks[index] ? 'bg-green-300' : 'hover:bg-stone-200'} transform transition-transform duration-200 ease-in-out hover:scale-105`}
+      <button
+        onTouchStart={handleTouchStart}
+        onTouchEnd={handleTouchEnd}
+        onTouchCancel={handleTouchEnd}
+        className={buttonClassName}
       >
         {isChecked && <span className="mr-2 ml-2 text-green-800 text-2xl">✓</span>}
         <span className={`${checkedTasks[index] ? 'line-through text-green-800' : ''} ml-4`}>{task}</span>
-        {checkedTasks[index] && <span className="text-green-500 text-xl absolute top-0 right-0 mr-2 mt-2">Quest Complete</span>}
-        {isMouseDown && (
+        {checkedTasks[index] && (
+          <span className="text-green-500 text-xl absolute top-0 right-0 mr-2 mt-2">Quest Complete</span>
+        )}
+        {isTouchDown && (
           <animated.div
             style={{
               ...fillAnimation,
