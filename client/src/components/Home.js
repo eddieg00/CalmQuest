897ae45edece0ecce52d965970ae9cec0b6206e3
Michael <michaelmees99@gmail.com>
2023-06-12 15:32:12 -0400

    home mobile broken but works on desktop

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -70,18 +70,14 @@ export const Home = () => {
 
   return (
     <>
-
-      {logged ?(
-        <div className="lg: flex flex-col w-screen min-h-screen relative">
+      {logged ? (
+        <div className="flex flex-col w-screen min-h-screen relative">
           <canvas id="my-canvas" ref={canvasRef} className="w-full h-full absolute top-0 left-0" />
-          <div className="lg:flex flex-grow relative">
-            <div className="lg: bg-white w-3/10 p-8 flex flex-col justify-between h-screen">
+          <div className="flex flex-col lg:flex-row flex-grow relative">
+            <div className="bg-white w-full lg:w-2/12 p-4 lg:p-8 flex flex-col justify-between h-screen">
               <div>
-                <h2 className="lg: text-3xl mb-4 text-emerald-500 font-nexa font-bold">CalmQuest</h2>
-                {/*
-                <button className="rounded-2xl m-2 text-white bg-blue-400 w-2/5 px-2 py-2 shadow-md hover:text-blue-400 hover:bg-white transition duration-200 ease-in" onClick={openModal}>Profile</button>
-                */}
-                <div className='lg: flex'>
+                <h2 className="text-2xl lg:text-3xl mb-4 text-emerald-500 font-nexa font-bold">CalmQuest</h2>
+                <div className='flex flex-col lg:flex-row'>
                   <HealthResourceList healthResources={healthResources} handleResourceClick={handleResourceClick} search={search} handleSearch={handleSearch}/>
                 </div>
               </div>
@@ -91,23 +87,23 @@ export const Home = () => {
                 </a>
               </p>
             </div>
-      
-            <div className="lg: w-full w-7/10 flex flex-col items-start justify-start p-8">
-              <div className="lg: bg-white bg-opacity-80  rounded-lg shadow p-8 w-full overflow-auto mb-6">
-                <h1 className="lg: text-4xl mb-4 text-center">Welcome to <span className="text-5xl font-nexa font-bold text-emerald-500">CalmQuest</span></h1>
-                <p className="lg: text-2xl text-center justify-center bg-gradient-to-l from-emerald-600 via-emerald-500 to-emerald-600 bg-clip-text text-transparent font-nexa font-bold">Daily Quests:</p>
-      
+  
+            <div className="w-full lg:w-10/12 p-4 lg:p-8 flex flex-col items-start justify-start">
+              <div className="bg-white bg-opacity-80  rounded-lg shadow p-4 lg:p-8 w-full overflow-auto mb-6">
+                <h1 className="text-2xl lg:text-4xl mb-4 text-center">Welcome to <span className="text-3xl lg:text-5xl font-nexa font-bold text-emerald-500">CalmQuest</span></h1>
+                <p className="text-xl lg:text-2xl text-center justify-center bg-gradient-to-l from-emerald-600 via-emerald-500 to-emerald-600 bg-clip-text text-transparent font-nexa font-bold">Daily Quests:</p>
+  
                 <TaskList />
               </div>
-              <div className=" w-full p-8 bg-opacity-80  bg-white rounded-lg shadow">
-                <p className="text-2xl text-center bg-gradient-to-l from-emerald-600 via-emerald-500 to-emerald-600 bg-clip-text text-transparent font-nexa font-bold">Today's Inspiring Quote:</p>
-                <p className="font-nexa font-ultralight text-3xl italic text-center ">
+              <div className=" w-full p-4 lg:p-8 bg-opacity-80  bg-white rounded-lg shadow">
+                <p className="text-xl lg:text-2xl text-center bg-gradient-to-l from-emerald-600 via-emerald-500 to-emerald-600 bg-clip-text text-transparent font-nexa font-bold">Today's Inspiring Quote:</p>
+                <p className="font-nexa font-ultralight text-2xl lg:text-3xl italic text-center ">
                   {quoteData.quote ? `“${quoteData.quote}” - ${quoteData.author}` : 'Loading...'}
                 </p>
               </div>
             </div>
           </div>
-      
+  
           {modalOpen && (
             <div
               style={{
@@ -122,15 +118,13 @@ export const Home = () => {
                 background: 'rgba(0, 0, 0, 0.5)',
               }}
             >
-
-                  <p>
-                    <a href="/login" onClick={() => Auth.logout()}>Logout</a>
-                  </p>
-
+              <p>
+                <a href="/login" onClick={() => Auth.logout()}>Logout</a>
+              </p>
             </div>
           )}
         </div>
       ) : null}
-    </>  
+    </>
   );
 };
